46e05e03fa79573cabbdb70facb977c6db7c147c
Marishka M <mmagness@student.unimelb.edu.au>
2020-04-20 13:04:58 +1000

    added security with dotenv

--- models/db.js
+++ models/db.js
@@ -1,21 +1,28 @@
+require('dotenv').config()
 const mongoose = require('mongoose');
 
 // copy from CONNECT (MongoDB Atlas)
 const dbURI =
-'mongodb+srv://HydroHomies:ItYwbp104PVCu5ne@hydrohomies-u3qcl.mongodb.net/test?retryWrites=true&w=majority';
+  'mongodb+srv://HydroHomies:<password>@hydrohomies-u3qcl.mongodb.net/test?retryWrites=true&w=majority';
+
+var dbURL =
+  dbURI.replace('<password>', process.env.MONGO_PASSWORD);
 
 const options = {
-	useNewUrlParser: true,
-	dbName: 'Databace'
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  dbName: 'Databace'
 };
 
-mongoose.connect(dbURI, options).then(
- () => {
-	console.log('Database connection established!');
-},
- err => {
-	console.log('Error connecting Database instance due to: ', err);
-});
+mongoose.connect(dbURL, options).then(
+  () => {
+    console.log('Database connection established!');
+  },
+  err => {
+    console.log('Error connecting Database instance due to: ', err);
+  });
 
 // Initializes the schema for mongodb
 require('./user.js');
