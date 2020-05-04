f58cb926cdd8e38d243945ce4c5a863f80b67898
Marishka M <mmagness@student.unimelb.edu.au>
2020-05-04 12:18:41 +1000

    routes for controllers

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
-var path = require('path');
+var path = require('path
+var loginController = require('../controllers/loginController.js');
+var imageController = require('../controllers/imageController.js');
 
+/* GET Requests */
+
+// Get homepage
 router.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, '/../views/homepage.html'));
+	if (!req.session.user) {
+		res.sendFile(path.join(__dirname, '/../views/homepage/homepage.html'));
+	} else {
+		res.redirect('/main');
+	}
+  // dirname : It will resolve to your project folder.
 });
+
+/* POST requests */
+
+// Login
+router.post('/login', loginController.login);
+
+// Logout
+router.post('/logout', loginController.logout);
+
+// Register
+router.post('/register', loginController.register);
+
 
 module.exports = router;  
