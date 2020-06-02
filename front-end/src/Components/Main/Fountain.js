a250e35e118ce4610ffa7dee9aaa95c70b316fd4
daltavida <assin@student.unimelb.edu.au>
2020-06-02 13:15:53 +1000

    Update Fountain.js

--- front-end/src/Components/Main/Fountain.js
+++ front-end/src/Components/Main/Fountain.js
@@ -1,21 +1,15 @@
 import React from "react";
-import Button from 'react-bootstrap/Button';
+import Carousel, { Dots } from '@brainhubeu/react-carousel';
+import '@brainhubeu/react-carousel/lib/style.css';
 
 export const Fountain = () => (
-
-    <div className="fountain">
-        <h1> Welcome to Fountain Page </h1>
-        <br></br>
-        <br></br>
-        {/* Create links to take user to the signup page*/}
-        <h5> New to Hydrohomies</h5>
-        <a href = "./Login"><Button size ="lg">Login</Button> </a>
-        <br></br>
-        <br></br>
-        <h3> OR </h3>
-        <br></br>
-        {/* Create links to take user to the login page*/}
-        <h5>Already a user</h5>
-        <a href = "./SignUp"> <Button  size = "lg">Sign Up</Button> </a>
-    </div>
+    <Carousel
+        slidesPerPage={2}
+        arrows
+        infinite
+    >
+        <img src={"https://instagram.fsyd3-1.fna.fbcdn.net/v/t51.2885-15/e35/89271564_197225711691075_6216866783474562588_n.jpg?_nc_ht=instagram.fsyd3-1.fna.fbcdn.net&_nc_cat=103&_nc_ohc=HY6gtbMwpvsAX8ujIro&oh=42acc7b0453fa1ebebe6e4b48e6d3927&oe=5F00BBEA"} />
+        <img src={"https://instagram.fsyd3-1.fna.fbcdn.net/v/t51.2885-15/e35/87225050_182458953086637_2311254261082711070_n.jpg?_nc_ht=instagram.fsyd3-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=jlSsj9rHZeQAX_f4L0R&oh=a58d38574b2de731ccc50ad21dc39ecc&oe=5F013BBD"} />
+        <img src={"https://instagram.fsyd3-1.fna.fbcdn.net/v/t51.2885-15/e35/83084857_205984013876290_7098924246053673749_n.jpg?_nc_ht=instagram.fsyd3-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=ZguV_ecp-kcAX9rQrfp&oh=77755bf0555676d638f0b09a2aff1e0d&oe=5EFE8B27"} />
+    </Carousel>
 )
