eaa921f290d6f687b997083d208c43ab41db17cd
Audrey <audrey.vore12@gmail.com>
2020-06-07 13:36:21 +1000

    new landing page

--- front-end/src/Components/Main/Home.js
+++ front-end/src/Components/Main/Home.js
@@ -1,34 +1,63 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
+import Form from 'react-bootstrap/Form';
+import Wave from 'react-wavify';
 import "./Home.css";
 
 export const Home = () => (
   <div className="home">
-    <h1> Welcome to Hydrohomies </h1>
 
-    <div className="section">
-    {/* Create links to take user to the signup page*/}
-        <br/><br/>
-    <div className="signup">
-    <h5> New to Hydrohomies</h5>
-    <a href="./SignUp">
+  {/*create header */}
+   <header className = "Hydrohomies">
+     <h1> Hydrohomies</h1> 
+     <p className = "smallheader">We are water</p>
+    </header>
 
-      <Button size="lg">Sign Up</Button>
-    </a>
-    </div>
+{/*tagline for the website*/}
+    <div className = "wave">
+      <div className = "tagline">
+        <span> Don't </span>
+        <br></br>
+        let life
+        <br></br>
+        slip through the
+        <br></br>
+        drain
+      </div>
 
+{/*create form for login and sig up options */}
+      <div className = "form-wrapper">
+        <h2 className = "formheader"> Welcome to hydrohomies</h2>
+        <br></br>
+        <Form className = "landing">
+          <Form.Group controlId="formBasicEmail">
+             <Form.Label className = "form1">New to hydrohmies</Form.Label>
+{/*create links to signup and login page */}
+             <a href = "./SignUp">
+              <Button variant="outline-secondary" size = "lg" >Create an account</Button>
+            </a>
+          </Form.Group>
+          <br></br>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label className = "form2">Already a member</Form.Label>
+            <a href = "./Login">
+              <Button variant="outline-secondary" size = "lg" >Log In</Button>
+            </a>
+          </Form.Group>
+        </Form>
+      </div>
+      <br></br>
 
-    <br/><br/>
-    <h3> OR </h3>
+{/*create wave for page */}
 
-    {/* Create links to take user to the login page*/}
-    <div className="login">
-        <br/><br/>
-    <h5>Already a user</h5>
-    <a href="./Login">
-      <Button size="lg">Login</Button>
-    </a>
-    </div>
+  <Wave className = "bottomwave" fill="url(#gradient)" options={{ points: 3, speed: 0.27, amplitude: 40, height: 40}}>
+  <defs>
+    <linearGradient id="gradient" gradientTransform="rotate(90)">
+      <stop offset="10%"  stopColor="#2196f3" />
+      <stop offset="90%" stopColor="#57E0E5" />
+    </linearGradient>
+  </defs>
+</Wave>
     </div>
   </div>
 );
